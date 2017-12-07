49f514b0d7ecb19e3f0cb09044924cd69af6f4a1
Dmitry <zzzio@mail.ru>
2017-12-07 22:43:47 +0400

    lesson09

--- lesson_09/myScript2.js
+++ lesson_09/myScript2.js
@@ -1,34 +1,28 @@
-var regexp = /[\s\.\?\,\;\:\!]/g;
-
 function remove(string){
-  loweString = string.toLowerCase();
+  var regexp = /[\s\.\?\,\;\:\!]/g;
+  var loweString = string.toLowerCase();
   var words = loweString.split(regexp);
   var b;
   var equals = [];
+  var chars = words[0].split("");
 
-  // for (var i = 0; i < words.length; i++) {
-    var chars = words[0].split("");
-    for (var j = 0; j < chars.length; j++) {
-      for (var k = 0; k < words.length; k++) {
-        if (words[k].length > 0) {
-          if (words[k].indexOf(chars[j]) === -1) {
-            b = false;
-            break;
-          } else {
-            b = true;
-          }
+  for (var j = 0; j < chars.length; j++) {
+    for (var k = 0; k < words.length; k++) {
+      if (words[k].length > 0) {
+        if (words[k].indexOf(chars[j]) === -1) {
+          b = false;
+          break;
+        } else {
+          b = true;
         }
       }
-      if (b) {
-        equals.push(chars[j]);
-      }
     }
-
-  // }
-  for (var i = 0; i < equals.length; i++) {
-    string.replace(new RegExp(equals[i], 'g'), '');
+    if (b) {
+      equals.push(chars[j]);
+    }
   }
-  console.log(string);
-};
-
-remove("Чего-с изволите-с?Барин-с!");
+  var rgsp = "[" + equals + "]";
+  var result = string.replace(new RegExp(rgsp, "ig"), "");
+  return result;
+  };
+  console.log(remove("Чего-с изволите-с?Барин-с!"));
